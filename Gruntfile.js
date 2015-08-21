6c851655b9e2bef601c613c2a2a06b12a16df9a2
Hao Tri Pham <hao.tripham@jumio.com>
2015-08-21 19:24:21 +0200

    Basic build & run setup: - clean - JSHint - ngTemplates - express - watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,8 +13,8 @@ module.exports = function(grunt) {
     pkg: pkg,
     app: {
       src: 'src',
-      thirdparty: 'thirdparty',
-      bower_components: 'thirdparty/bower_components',
+      third: 'thirdparty',
+      bower: 'thirdparty/bower_components',
       test: 'test',
       temp: '.temp',
       dist: 'dist'
@@ -23,26 +23,82 @@ module.exports = function(grunt) {
       install: {
         options: {
           install: true,
-          //targetDir: '<%= app.bower_components %>', -- not necessary, since 'bower install' is executed and will read from .bowerrc config file
+          //targetDir: '<%= app.bower %>/', -- not necessary, since 'bower install' is executed and will read from .bowerrc config file
           verbose: true,
           copy: false
         }
       }
     },
-    uglify: {
+    clean: {
+      all: {
+        files: [{
+          dot: true,
+          src: [
+            '<%= app.temp %>',
+            '<%= app.dist %>'
+          ]
+        }]
+      }
+    },
+    jshint: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        jshintrc: '.jshintrc'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'dist/<%= pkg.name %>.min.js'
+      all: [
+        'Gruntfile.js',
+        '<%= app.src %>/**/*.js',
+        '<%= app.test %>/**/*.js'
+      ]
+    },
+    ngtemplates: {
+      dev: {
+        src: '<%= app.src %>/ui/**/*.html',
+        dest: '<%= app.temp %>/scripts/templates.js',
+        options: {
+          module: 'journ-me-client',
+          url: function(url) {
+            return url.replace(/(src\/ui\/([\s\S]*?)\/)/, '').replace(/.html/, '');
+          }
+        }
+      }
+    },
+    express: {
+      dev: {
+        options: {
+          port: 9000,
+          hostname: '0.0.0.0',
+          bases: [
+            '<%= app.src %>',
+            '<%= app.temp %>'
+          ],
+          //server: '<%= app.test %>/server/serverMock.js', -- setting up server mock responses later
+          livereload: true
+        }
+      }
+    },
+    watch: {
+      ngTemplates: {
+        files: ['<%= app.src %>/ui/**/*.html'],
+        tasks: ['ngtemplates']
       }
     }
   });
 
   // GruntJS task registration
+  grunt.registerTask('build', [
+    'clean',
+    'jshint',
+    'ngtemplates:dev'
+  ]);
+
+  grunt.registerTask('run', [
+    'build',
+    'express',
+    'watch'
+  ]);
+
   grunt.registerTask('default', [
     'bower',
-    'uglify'
+    'run'
   ]);
 };
